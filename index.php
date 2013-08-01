cf0d4ca07ed5a3dbc8a626d8bfb505fd71d40e3c
Daniel Eliasson <daniel@stilero.com>
2013-08-01 13:45:02 +0200

    timelines added

--- index.php
+++ index.php
@@ -15,11 +15,12 @@ foreach (glob("./classes/*.php") as $filename)
 $Consumer = new OauthClient("A7NX7yIAGXuk3CDvTEDzLg", "Nnz3HrNjNCE6inhGVTjA6FWl9lleYnL2gUu6lf2PZd0");
 $User = new OauthUser("19602888-A3bNfPSzZzca6ydGmL3oN5fMyAnk0RpLtxA7vbNri", "Abkw6VwuS9rdQ54JwSchkfnCibP4gWmKkunda5dBWao");
 //$Server = new OauthServer($Consumer, $User);
-$Tweets = new TwitterTweets($Consumer, $User);
+$Tweets = new TwitterTimelines($Consumer, $User);
 //$json = $Tweets->update("Testing my new app");
 //$json = $Tweets->destroy('362893768636108801');
-$json = $Tweets->retweet('1234567890');
+//$json = $Tweets->retweet('1234567890');
 //$json = $Tweets->showRetweets('1234567890');
+$json = $Tweets->home();
 $json_array = json_decode($json);
 ?>
 <pre>
