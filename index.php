c3b913e6fe31265b1a39d796ccf293a47f2dfc13
Daniel Eliasson <daniel@stilero.com>
2013-12-31 12:39:05 +0100

    cleaned up the index

--- index.php
+++ index.php
@@ -36,13 +36,11 @@ foreach (glob(PATH_TWITTER_HELPERS."*.php") as $filename){
 require_once dirname(__FILE__).'/jerror.php';
 $consumerKey = 'zWUbW6ozHQPnPidtIWXjyw';
 $consumerSecret = 'xHV2gQGjmyDCX2iwBRKLq1sQ6TWJxNfqhj7Le62G26A';
-$Consumer = new StileroTwitterOauthConsumer($consumerKey, $consumerSecret);
 $token = '19602888-zGpIeTYRIkeXPiRYXK5uJUyrfx67pSstMnkeG9Q1Q';
 $tokenSecret = 'HmnBI1xX0ERQxHV9vjTO79XdCxi8LgUOYpDIihbDeDo';
-$Access = new StileroTwitterOauthAccess($token, $tokenSecret);
-$imagefile = dirname(__FILE__).'/joomla_logo_black.jpg';
 $Twitter = new StileroTwitter($consumerKey, $consumerSecret, $token, $tokenSecret);
 $json = $Twitter->Tweets->update('Testing with images');
 $response = StileroTwitterResponse::handle($json);
+$imagefile = dirname(__FILE__).'/joomla_logo_black.jpg';
 var_dump($response);exit;
 ?>
